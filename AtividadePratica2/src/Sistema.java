94db705da6685d353a256737dc4e2062e6d496ee
malobr <marcelotc1202@gmail.com>
2024-04-23 20:05:21 -0300

    IMplementacao dos desenhos

--- AtividadePratica2/src/Sistema.java
+++ AtividadePratica2/src/Sistema.java
@@ -9,9 +9,14 @@ public class Sistema {
     private static List<Funcionario> listaFuncionarios = new ArrayList<>();
 
     private static void exibirMenu() {
-        System.out.println("---------------");
-        System.out.println("|     BOATE    |");
-        System.out.println("---------------");
+        System.out.println("\n");
+        System.out.println(" ──────▄▀──                       ──────▄▀──");
+        System.out.println("─█▀▀▀█▀█─     ---------------     ─█▀▀▀█▀█─");
+        System.out.println("──▀▄░▄▀──     |     BOATE    |    ──▀▄░▄▀──");
+        System.out.println("────█────     ---------------     ────█────");
+        System.out.println("──▄▄█▄▄──                         ──▄▄█▄▄──");
+
+
         System.out.println("\nClientes:");
         System.out.println("[1] Cadastrar Cliente");
         System.out.println("[2] Procurar Cliente");
@@ -23,7 +28,7 @@ public class Sistema {
         System.out.println("[7] Excluir Funcionario");
         System.out.println("[8] Listar Todos os Funcionarios");
         System.out.println("0) Sair");
-        System.out.print("Informe uma opção: ");
+        System.out.print("\nInforme uma opção: ");
     }
 
     private static void verificarOpcao(int op) {
@@ -56,7 +61,7 @@ public class Sistema {
                 System.out.println("\nO programa foi finalizado...");
                 break;
             default:
-                System.out.println("\nOpção inválida. Digite novamente.");
+                System.out.println("\nOpção invalida.... Digite novamente...");
                 break;
         }
     }
@@ -100,25 +105,25 @@ public class Sistema {
                 return;
             }
         }
-        System.out.println("Cliente não encontrado.");
+        System.out.println("Cliente nao encontrado.");
     }
     
     private static void excluirCliente() {
-        System.out.println("Informe o CPF do cliente a ser excluído:");
+        System.out.println("Informe o CPF do cliente a ser excluido:");
         int cpf = scanner.nextInt();
         for (Cliente cliente : listaClientes) {
             if (cliente.getCpf() == cpf) {
                 listaClientes.remove(cliente);
-                System.out.println("Cliente excluído com sucesso!");
+                System.out.println("Cliente excluido com sucesso!");
                 return;
             }
         }
-        System.out.println("Cliente não encontrado.");
+        System.out.println("Cliente nao encontrado.");
     }
     
     private static void listarClientes() {
         if (listaClientes.size() == 0) {
-            System.out.println("\nNão há clientes cadastrados.");
+            System.out.println("\nNao ha clientes cadastrados.");
         } else {
             System.out.println("\nLista de Clientes:");
             for (Cliente cliente : listaClientes) {
@@ -129,40 +134,40 @@ public class Sistema {
     
     
     private static void cadastrarNovoFuncionario() {
-       
+       ////////////////////Fazer depois.....
     }
     
     private static void procurarFuncionario() {
-        System.out.println("Informe o CPF do funcionário:");
+        System.out.println("Informe o CPF do funcionario:");
         int cpf = scanner.nextInt();
         for (Funcionario funcionario : listaFuncionarios) {
             if (funcionario.getCpf() == cpf) {
-                System.out.println("Funcionário encontrado:");
+                System.out.println("Funcionario encontrado:");
                 System.out.println(funcionario);
                 return;
             }
         }
-        System.out.println("Funcionário não encontrado.");
+        System.out.println("Funcionario não encontrado.");
     }
     
     private static void excluirFuncionario() {
-        System.out.println("Informe o CPF do funcionário a ser excluído:");
+        System.out.println("Informe o CPF do funcionario a ser excluido:");
         int cpf = scanner.nextInt();
         for (Funcionario funcionario : listaFuncionarios) {
             if (funcionario.getCpf() == cpf) {
                 listaFuncionarios.remove(funcionario);
-                System.out.println("Funcionário excluído com sucesso!");
+                System.out.println("Funcionario excluido com sucesso!");
                 return;
             }
         }
-        System.out.println("Funcionário não encontrado.");
+        System.out.println("Funcionario não encontrado.");
     }
     
     private static void listarFuncionarios() {
         if (listaFuncionarios.size() == 0) {
-            System.out.println("\nNão há funcionários cadastrados.");
+            System.out.println("\nNão há funcionarios cadastrados.");
         } else {
-            System.out.println("Lista de Funcionários:");
+            System.out.println("Lista de Funcionarios:");
             for (Funcionario funcionario : listaFuncionarios) {
                 System.out.println(funcionario);
             }
