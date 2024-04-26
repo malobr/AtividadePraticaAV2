90b66e030bd4c6d00dd07215001c58cfb3d34ac1
malobr <marcelotc1202@gmail.com>
2024-04-26 18:52:52 -0300

    Adicao de um comentario com o nome dos autores do codigo

--- AtividadePratica2/src/Main.java
+++ AtividadePratica2/src/Main.java
@@ -1,3 +1,6 @@
+/* Programa feito por Marcelo Tomas Cavalheiro e Lucca Cruz Pasqualli.
+ * Ambos estudantes de ADS - Analise e desenvolvimento de sistemas na UP Universidade Positivo...
+ */
 
 public class Main {
     public static void main(String[] args) {
